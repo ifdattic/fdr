269b846fd66c765c672a11f5b851df5b304de069
Andrew Marcinkevicius <andrew.web@ifdattic.com>
2015-05-07 11:45:09 +0300

    Add InMemoryUserRepository & action to retrieve all users

---
+++ src/AppBundle/Repository/InMemoryUserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Domain\Core\Identity\Uuid;
+use Domain\User\Entity\User;
+use Domain\User\Repository\UserRepository;
+use Domain\User\ValueObject\Email;
+use Domain\User\ValueObject\FullName;
+use Domain\User\ValueObject\UserId;
+
+class InMemoryUserRepository implements UserRepository
+{
+    private $users;
+
+    public function __construct()
+    {
+        $this->users[] = new User(
+            new UserId(new Uuid('8ce05088-ed1f-43e9-a415-3b3792655a9b')),
+            new Email('john@doe.com'),
+            new FullName('John Doe')
+        );
+
+        $this->users[] = new User(
+            new UserId(new Uuid('62a0ceb4-0403-4aa6-a6cd-1ee808ad4d23')),
+            new Email('jean@bon.com'),
+            new FullName('Jean Bon')
+        );
+    }
+
+    public function find(UserId $userId)
+    {
+
+    }
+
+    public function findAll()
+    {
+        return $this->users;
+    }
+
+    public function add(User $user)
+    {
+
+    }
+
+    public function remove(User $user)
+    {
+
+    }
+}
