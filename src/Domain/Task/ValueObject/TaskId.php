662c52c1f4efdcba50fa257f54208317df68723e
Andrew Marcinkevicius <andrew.web@ifdattic.com>
2015-05-27 12:35:03 +0300

    Add TaskId Value Object

---
+++ src/Domain/Task/ValueObject/TaskId.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Domain\Task\ValueObject;
+
+use Domain\Core\Identity\Uuid;
+
+final class TaskId
+{
+    /** @var Uuid */
+    private $uuid;
+
+    public function __construct(Uuid $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    /**
+     * @param  string $uuid
+     * @return self
+     */
+    public static function createFromString($uuid)
+    {
+        $uuid = new Uuid($uuid);
+
+        return new self($uuid);
+    }
+
+    /** @return string */
+    public function getValue()
+    {
+        return $this->uuid->getValue();
+    }
+
+    /** @return string */
+    public function __toString()
+    {
+        return $this->getValue();
+    }
+}
