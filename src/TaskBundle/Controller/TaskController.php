db88c38cd94fdccdf8c03aa7867dc61a027c02a5
Andrew Marcinkevicius <andrew.web@ifdattic.com>
2015-08-11 19:16:39 +0300

    Remove Nelmio API Docs bundle & documentation from controllers

--- src/TaskBundle/Controller/TaskController.php
+++ src/TaskBundle/Controller/TaskController.php
@@ -6,7 +6,6 @@ use AppBundle\Controller\ApiController;
 use Domain\Task\Command\DeleteTask;
 use Domain\Task\Command\GetTask;
 use Domain\Task\Command\GetTasksByUser;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use TaskBundle\Form\Type\CreateTaskCommandType;
@@ -14,62 +13,6 @@ use TaskBundle\Form\Type\UpdateTaskCommandType;
 
 class TaskController extends ApiController
 {
-    /**
-     * Create a new task.
-     *
-     * If task is **not completed it has no `completed_at`** in the response!
-     *
-     * #### Example of successful response
-     *
-     * ```
-     * {
-     *   "message": "Task created.",
-     *   "task": {
-     *     "id": "b4b60f6f-9454-4e03-b9fe-80066e9ac441",
-     *     "name": "Task name",
-     *     "date": "2015-06-28T00:00:00+0000",
-     *     "description": "This is task description",
-     *     "estimate": 3,
-     *     "completed": true,
-     *     "completed_at": "2015-05-05T05:05:05+0000",
-     *     "time_spent": 23,
-     *     "important": true,
-     *     "created_at": "2015-04-14T06:12:29+0000"
-     *   }
-     * }
-     * ```
-     *
-     * #### Example of failed validation response
-     *
-     * ```
-     * {
-     *   "errors": [
-     *     {
-     *       "message": "Task name is required",
-     *       "field": "name"
-     *     },
-     *     {
-     *       "message": "Date is required",
-     *       "field": "date"
-     *     }
-     *   ]
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     resource=true,
-     *     authentication=true,
-     *     input="TaskBundle\Form\Type\CreateTaskCommandType",
-     *     output="Domain\Task\Entity\Task",
-     *     statusCodes={
-     *         201="Returned when successful",
-     *         400={
-     *             "Returned when request is invalid",
-     *             "Returned when validation fails"
-     *         }
-     *     }
-     * )
-     */
     public function createAction(Request $request)
     {
         $user = $this->get('user_provider')->getUser();
@@ -99,58 +42,6 @@ class TaskController extends ApiController
         return $this->respondWithForm($form);
     }
 
-    /**
-     * Get data of a task.
-     *
-     * ### Example of successful response
-     *
-     * ```
-     * {
-     *   "task": {
-     *     "id": "5399dbab-ccd0-493c-be1a-67300de1671f",
-     *     "name": "Task Name",
-     *     "date": "2015-04-15",
-     *     "description": "This is the description.",
-     *     "estimate": 3,
-     *     "completed": true,
-     *     "completed_at": "2015-04-15T13:14:15+0000",
-     *     "time_spent": 23,
-     *     "important": true,
-     *     "created_at": "2015-06-18T07:13:26+0000"
-     *   }
-     * }
-     * ```
-     *
-     * ### Example of response when not an owner of a task
-     *
-     * ```
-     * {
-     *   "code": 403,
-     *   "message": "Access Denied",
-     *   "errors": null
-     * }
-     * ```
-     *
-     * ### Example when task is not found response
-     *
-     * ```
-     * {
-     *   "code": 404,
-     *   "message": "Task not found",
-     *   "errors": null
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     resource=true,
-     *     authentication=true,
-     *     statusCodes={
-     *         200="Returned when successful",
-     *         403="Returned when not an owner of a resource",
-     *         404="Returned when resource not found"
-     *     }
-     * )
-     */
     public function getAction($id)
     {
         $command = new GetTask($id);
@@ -166,58 +57,6 @@ class TaskController extends ApiController
         ;
     }
 
-    /**
-     * Get all user tasks.
-     *
-     * ### Example of response when user has tasks
-     *
-     * ```
-     * {
-     *   "tasks": [
-     *     {
-     *       "id": "5399dbab-ccd0-493c-be1a-67300de1671f",
-     *       "name": "Task Name",
-     *       "date": "2015-04-15",
-     *       "description": "This is the description.",
-     *       "estimate": 3,
-     *       "completed": true,
-     *       "completed_at": "2015-04-15T13:14:15+0000",
-     *       "time_spent": 23,
-     *       "important": true,
-     *       "created_at": "2015-06-20T07:21:54+0000"
-     *     },
-     *     {
-     *       "id": "df603d36-1203-4bc5-9cd8-99c775ac272a",
-     *       "name": "Task Name Alternative",
-     *       "date": "2014-06-05",
-     *       "description": null,
-     *       "estimate": 1,
-     *       "completed": false,
-     *       "completed_at": null,
-     *       "time_spent": 0,
-     *       "important": true,
-     *       "created_at": "2015-06-20T07:21:54+0000"
-     *     }
-     *   ]
-     * }
-     * ```
-     *
-     * ### Example of response when user has no tasks
-     *
-     * ```
-     * {
-     *   "tasks": []
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     resource=true,
-     *     authentication=true,
-     *     statusCodes={
-     *         200="Returned when successful"
-     *     }
-     * )
-     */
     public function getAllAction()
     {
         $user = $this->get('user_provider')->getUser();
@@ -234,45 +73,6 @@ class TaskController extends ApiController
         ;
     }
 
-    /**
-     * Delete a task.
-     *
-     * ### Example of successful response
-     *
-     * ```
-     * []
-     * ```
-     *
-     * ### Example resource if not an owner of task
-     *
-     * ```
-     * {
-     *   "code": 403,
-     *   "message": "Access Denied",
-     *   "errors": null
-     * }
-     * ```
-     *
-     * ### Example resource if task not found
-     *
-     * ```
-     * {
-     *   "code": 404,
-     *   "message": "Task not found",
-     *   "errors": null
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     resource=true,
-     *     authentication=true,
-     *     statusCodes={
-     *         200="Returned when successful",
-     *         403="Returned when not an owner of a resource",
-     *         404="Returned when resource not found"
-     *     }
-     * )
-     */
     public function deleteAction($id)
     {
         $command = new DeleteTask($id);
@@ -286,60 +86,6 @@ class TaskController extends ApiController
         ;
     }
 
-    /**
-     * Update an existing task.
-     *
-     * #### Example of successful response
-     *
-     * ```
-     * {
-     *   "message": "Task updated.",
-     *   "task": {
-     *     "id": "5399dbab-ccd0-493c-be1a-67300de1671f",
-     *     "name": "Task name 2",
-     *     "date": "2015-06-28",
-     *     "description": "This is task description",
-     *     "estimate": 3,
-     *     "completed": false,
-     *     "completed_at": null,
-     *     "time_spent": 23,
-     *     "important": true,
-     *     "created_at": "2015-08-08T11:52:58+0000"
-     *   }
-     * }
-     * ```
-     *
-     * #### Example of failed validation response
-     *
-     * ```
-     * {
-     *   "errors": [
-     *     {
-     *       "message": "Task name is required",
-     *       "field": "name"
-     *     },
-     *     {
-     *       "message": "Date is required",
-     *       "field": "date"
-     *     }
-     *   ]
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     resource=true,
-     *     authentication=true,
-     *     input="TaskBundle\Form\Type\UpdateTaskCommandType",
-     *     output="Domain\Task\Entity\Task",
-     *     statusCodes={
-     *         201="Returned when successful",
-     *         400={
-     *             "Returned when request is invalid",
-     *             "Returned when validation fails"
-     *         }
-     *     }
-     * )
-     */
     public function updateAction(Request $request, $id)
     {
         $command = new GetTask($id);
--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -4,85 +4,17 @@ namespace UserBundle\Controller;
 
 use AppBundle\Controller\ApiController;
 use Domain\User\Command\GetUser;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use UserBundle\Form\Type\SignUpCommandType;
 
 class UserController extends ApiController
 {
-    /**
-     * Get authentication token.
-     *
-     * #### Example of invalid response
-     *
-     * ```
-     * {
-     *   "code": 401,
-     *   "message": "Bad credentials"
-     * }
-     *
-     * ```
-     *
-     * #### Example of successful response
-     *
-     * ```
-     * {
-     *   "token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE0NDA0MTMzOTEsInVzZXJuYW1lIjoidmlyZ2lsQG11bmRlbGwuY29tIiwiaWF0IjoiMTQzMjYzNzM5MSJ9.FlmdaElgoaXZS-udPi5SN6P6R7ftHX58UibxXdEI8Qyq86l_AirbSUrMy3PEKAdQH7tn11PohQLdJ0ktswhR5GLRo3xJgljyGVjRk3IcMT4NQ8nRecjlDHUUWVQ-Mc4wB3Mkca4-cx--57c_XwZuwprl_PyKS8OvULludIyo1gFIzCIPS1zj_auTTArnQBhUZH33QxtpsMBpnHL97HJKAZgyITfT8g97QrPbIgiTw9SZRwe4F5jBeh5E1_f8VxMGCJ7NifmcrqhjXHW4SPJa_8Mvhxec89j2VnWi3gVlu6lfTfmf8a3RMHIppuQ3EpJtzzu8F8PulILhUmsygBkFh1BkaTrxGvJRuvIR_niBf1RVdB1E8W3kV4JUzmp86tdC0HesB_MCTgymmNO3oXq9mIeo4zJ65CWiDnsbNbohJ-qWpGSbGGMky3L2bDrm1TomAr4wf75FTn09vw3fJWuUI0_uOXoFUdcEMOaCwEI_otdt1ryeblQgtTclmcDN7-k8E7sN2y9YOxcKbdTHVvwWT5mzi_0pKSKholZXE6R1J7YYDFAsA6cn80DQ4nMLCqqNzm5DENnjWddOOuKV0h_SG25A4b1xBOVYXk2-6g-2csq45SIO1wQauU4Bz6kvj4GKNgdV7DGkstJeSkrExPyGt79GMIO684xwPwmL6frihBQ"
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     parameters={
-     *         {"name"="email", "dataType"="string", "required"=true, "description"="Email to authenticate with"},
-     *         {"name"="password", "dataType"="string", "required"=true}
-     *     },
-     *     statusCodes={
-     *         200="Returned when successful",
-     *         401="Returned when authentication fails"
-     *     },
-     *     tags={"info"="#0F6AB4"}
-     * )
-     */
     public function getTokenAction()
     {
         return new Response('', 401);
     }
 
-    /**
-     * Get data of currently logged in user.
-     *
-     * #### Example of invalid response
-     *
-     * ```
-     * {
-     *   "code": 401,
-     *   "message": "Invalid credentials"
-     * }
-     * ```
-     *
-     * #### Example of successful response
-     *
-     * ```
-     * {
-     *   "user": {
-     *     "id": "5399dbab-ccd0-493c-be1a-67300de1671f",
-     *     "email": "virgil@mundell.com",
-     *     "full_name": "Virgil Mundell"
-     *   }
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     resource=true,
-     *     authentication=true,
-     *     statusCodes={
-     *         200="Returned when successful",
-     *         401="Returned when authentication fails"
-     *     },
-     *     tags={"info"="#0F6AB4"}
-     * )
-     */
     public function getLoggedInUserDataAction()
     {
         $userProvider = $this->get('user_provider');
@@ -101,78 +33,6 @@ class UserController extends ApiController
         ;
     }
 
-    /**
-     * Sign up for an account.
-     *
-     * **After successful response the application should make a call for authentication.**
-     *
-     * #### Example of successful response
-     *
-     * ```
-     * {
-     *   "message": "Sign up successful."
-     * }
-     * ```
-     *
-     * #### Example of invalid form response
-     *
-     * ```
-     * {
-     *   "code": 400,
-     *   "message": "Bad Request"
-     * }
-     * ```
-     *
-     * #### Example of failed validation response
-     *
-     * ```
-     * {
-     *   "errors": [
-     *     {
-     *       "message": "The \"v\" is not a valid email",
-     *       "field": "email",
-     *       "code": 1
-     *     },
-     *     {
-     *       "message": "The full name is required",
-     *       "field": "full_name"
-     *     },
-     *     {
-     *       "message": "Password must be at least 8 characters",
-     *       "field": "password",
-     *       "code": 1
-     *     }
-     *   ]
-     * }
-     * ```
-     *
-     * #### Example of existing account response
-     *
-     * ```
-     * {
-     *   "errors": [
-     *     {
-     *       "message": "User with this email already exists",
-     *       "field": "email"
-     *     }
-     *   ]
-     * }
-     * ```
-     *
-     * @ApiDoc(
-     *     input="UserBundle\Form\Type\SignUpCommandType",
-     *     output="Domain\User\Entity\User",
-     *     statusCodes={
-     *         201="Returned when successful",
-     *         400={
-     *             "Returned when request is invalid",
-     *             "Returned when validation fails",
-     *             "Returned when account exists"
-     *         }
-     *     },
-     *     tags={"info"="#0F6AB4"}
-     * )
-     */
     public function signUpAction(Request $request)
     {
         $form = $this->createForm(new SignUpCommandType());
